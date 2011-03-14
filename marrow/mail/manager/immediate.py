9c4db0d8fa0fe49572f7cb80a27415e5468b8d5b
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-03-14 14:47:21 -0400

    Immediate manager.

--- marrow/mail/manager/immediate.py
+++ marrow/mail/manager/immediate.py
@@ -6,14 +6,25 @@ log = __import__('logging').getLogger(__name__)
 
 
 class ImmediateManager(object):
-	def __init__(self, **kw):
-		self.config = kw
-	
-	def startup(self):
-		pass
-	
-	def __call__(self, message):
-		pass # Deliver the message.
-	
-	def shutdown(self):
-		pass
+    def __init__(self, config, transport):
+        self.config = config
+        self.transport = transport()
+        
+        super(ImmediateManager, self).__init__()
+    
+    def startup(self):
+        log.info("Immediate delivery manager starting.")
+        
+        self.transport.startup()
+        
+        log.info("Immediate delivery manager started.")
+    
+    def deliver(self, message):
+        self.transport.deliver(message)
+    
+    def shutdown(self):
+        log.info("Immediate delivery manager stopping.")
+        
+        self.transport.shutdown()
+        
+        log.info("Immediate delivery manager stopped.")
