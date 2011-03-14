62a4bc77a92882fd9753060f0f1294b4d36df53c
Alex Grönholm <alex.gronholm+git@nextday.fi>
2011-03-14 18:21:18 +0200

    PEP-8'ified setup.py, fixed classifiers and added distribute_setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys, os
+import sys
+import os
 
-try:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-
-except ImportError:
-    pass
+from distribute_setup import use_setuptools
+use_setuptools()
 
 from setuptools import setup, find_packages
 
@@ -19,42 +16,48 @@ if sys.version_info < (2, 6):
 exec(open(os.path.join("marrow", "tags", "release.py")))
 
 
-
 setup(
-        name = "marrow.mail",
-        version = version,
-        
-        description = "A highly efficient and modular mail delivery framework for Python 2.6+ and 3.1+, formerly.",
-        author = "Alice Bevan-McGregor",
-        author_email = "alice+marrow@gothcandy.com",
-        url = "",
-        download_url = "",
-        license = "MIT",
-        keywords = '',
-        
-        install_requires = ['marrow.util'],
-        
-        test_suite = 'nose.collector',
-        tests_require = ['nose', 'coverage'],
-        
-        classifiers = [
-                "Development Status :: 1 - Planning",
-                "Environment :: Console",
-                "Intended Audience :: Developers",
-                "License :: OSI Approved :: MIT License",
-                "Operating System :: OS Independent",
-                "Programming Language :: Python",
-                "Topic :: Internet :: WWW/HTTP :: WSGI",
-                "Topic :: Software Development :: Libraries :: Python Modules"
-            ],
-        
-        packages = find_packages(exclude=['examples', 'tests', 'tests.*', 'docs', 'third-party']),
-        include_package_data = True,
-        package_data = {
-                '': ['README.textile', 'LICENSE', 'distribute_setup.py'],
-                'docs': ['Makefile', 'source/*']
-            },
-        zip_safe = True,
-        
-        namespace_packages = ['marrow']
-    )
+    name="marrow.mail",
+    version=version,
+
+    description="""
+A highly efficient and modular mail delivery framework for
+Python 2.6+ and 3.1+, formerly called Turbomail.""",
+    author="Alice Bevan-McGregor",
+    author_email="alice+marrow@gothcandy.com",
+    url="",
+    download_url="",
+    license="MIT",
+    keywords="",
+
+    install_requires=["marrow.util"],
+
+    test_suite="nose.collector",
+    tests_require=["nose", "coverage"],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+
+    packages=find_packages(exclude=["examples", "tests", "tests.*",
+                                    "docs", "third-party"]),
+    include_package_data=True,
+    package_data={
+        "": ["README.textile", "LICENSE", "distribute_setup.py"],
+        "docs": ["Makefile", "source/*"]
+    },
+    zip_safe=True,
+
+    namespace_packages=["marrow"]
+)
