1d587f8c9df2fa8ca165b87124ad7c6e85004b38
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-04-04 12:15:00 -0700

    Re-arrangement and slightly different __init__ structure.

--- marrow/mailer/message.py
+++ marrow/mailer/message.py
@@ -22,108 +22,114 @@ __all__ = ['Message']
 
 class Message(object):
     """Represents an e-mail message."""
-
+    
+    sender = AutoConverter(Address)
+    author = AutoConverter(AddressList, [])
+    to = AutoConverter(AddressList, [])
+    bcc = AutoConverter(AddressList, [])
+    cc = AutoConverter(AddressList, [])
+    reply = AutoConverter(AddressList, [])
+    notify = AutoConverter(AddressList, [])
+    
     def __init__(self, author=None, to=None, subject=None, **kw):
         """Instantiate a new Message object.
-
+        
         No arguments are required, as everything can be set using class
         properties.  Alternatively, __everything__ can be set using the
         constructor, using named arguments.  The first three positional
         arguments can be used to quickly prepare a simple message.
         """
-        self.sender = kw.pop('sender', None)
-        self.author = kw.pop('author', None)
-        self.to = kw.pop('to', None)
-        self.cc = kw.pop('cc', None)
-        self.bcc = kw.pop('bcc', None)
-        self.reply = kw.pop('reply', None)
-        self.notify = kw.pop('disposition', None)
-
-        self.subject = kw.pop('subject', None)
-        self.date = kw.pop('date', None)
-
-        self.encoding = kw.pop('encoding', 'utf-8')
-
-        self.organization = kw.pop('organization')
-        self.priority = kw.pop('priority')
-
-        self.plain = kw.pop('plain', None)
-        self.rich = kw.pop('rich', None)
-        self.attachments = kw.pop('attachments', [])
-        self.embedded = kw.pop('embedded', [])
-        self.headers = kw.pop('headers', [])
-        self.nr_retries = kw.pop('nr_retries', 3)
-
-        self._id = kw.get('id', None)
-
+        
+        # Internally used attributes.
+        
+        self._id = None
         self._processed = False
         self._dirty = False
-        if len(kw) > 0:
-            parameter_name = kw.keys()[0]
-            error_msg = "__init__() got an unexpected keyword argument '%s'"
-            raise TypeError(error_msg % parameter_name)
-
-    sender = AutoConverter(Address)
-    author = AutoConverter(AddressList)
-    to = AutoConverter(AddressList)
-    bcc = AutoConverter(AddressList)
-    cc = AutoConverter(AddressList)
-    reply = AutoConverter(AddressList)
-    notify = AutoConverter(AddressList)
-
+        
+        # Default values.
+        
+        self.subject = None
+        self.date = None
+        self.encoding = 'utf-8'
+        self.organization = None
+        self.priority = None
+        self.plain = None
+        self.rich = None
+        self.attachments = []
+        self.embedded = []
+        self.headers = []
+        self.retries = 3
+        
+        # Overrides at initialization time.
+        
+        if author is not None:
+            self.author = author
+        
+        if to is not None:
+            self.to = to
+        
+        if subject is not None:
+            self.subject = subject
+        
+        for k in kw:
+            if not hasattr(self, k):
+                raise TypeError("Unexpected keyword argument: %s" % k)
+            
+            setattr(self, k, kw[k])
+    
     def __setattr__(self, name, value):
         """Set the dirty flag as properties are updated."""
         object.__setattr__(self, name, value)
         if name not in ('bcc', '_id', '_dirty', '_processed'):
             object.__setattr__(self, '_dirty', True)
-
+    
     def __str__(self):
         return self.mime.as_string()
-
+    
     @property
     def id(self):
         if not self._id or (self._processed and self._dirty):
             self._id = make_msgid()
             self._processed = False
         return self._id
-
+    
     @property
     def envelope_sender(self):
         """Returns the address of the envelope sender address (SMTP from, if
         not set the sender, if this one isn't set too, the author)."""
         return self.sender or self.author[0]
-
+    
     @property
     def recipients(self):
         return AddressList(self.to + self.cc + self.bcc)
-
+    
     def mime_document(self, plain, rich=None):
         if not rich:
             message = plain
-
+        
         else:
             message = MIMEMultipart('alternative')
             message.attach(plain)
-
+            
             if not self.embedded:
                 message.attach(rich)
-
+            
             else:
                 embedded = MIMEMultipart('related')
                 embedded.attach(rich)
                 for attachment in self.embedded:
                     embedded.attach(attachment)
                 message.attach(embedded)
-
+        
         if self.attachments:
             attachments = MIMEMultipart()
             attachments.attach(message)
             for attachment in self.attachments:
                 attachments.attach(attachment)
             message = attachments
-
+        
         return message
-
+    
     def _build_date_header_string(self, date_value):
         """Gets the date_value (may be None, basestring, float or
         datetime.datetime instance) and returns a valid date string as per
@@ -133,7 +139,7 @@ class Message(object):
         if not isinstance(date_value, basestring):
             date_value = formatdate(date_value, localtime=True)
         return date_value
-
+    
     def _build_header_list(self, author, sender):
         date_value = self._build_date_header_string(self.date)
         headers = [
@@ -148,14 +154,14 @@ class Message(object):
                 ('Organization', self.organization),
                 ('X-Priority', self.priority),
             ]
-
+        
         if isinstance(self.headers, dict):
             for key in self.headers:
                 headers.append((key, self.headers[key]))
         else:
             headers.extend(self.headers)
         return headers
-
+    
     def _add_headers_to_message(self, message, headers):
         for header in headers:
             if isinstance(header, (tuple, list)):
@@ -171,7 +177,7 @@ class Message(object):
                 message[header[0]] = header[1]
             elif isinstance(header, dict):
                 message.add_header(**header)
-
+    
     @property
     def mime(self):
         """Produce the final MIME message."""
@@ -185,40 +191,40 @@ class Message(object):
             sender = []
         if not author:
             raise ValueError('You must specify an author.')
-
+        
         assert self.subject, "You must specify a subject."
         assert len(self.recipients) > 0, "You must specify at least one recipient."
         assert self.plain, "You must provide plain text content."
-
+        
         if len(author) > 1 and len(sender) == 0:
             raise ValueError('If there are multiple authors of message, you must specify a sender!')
         if len(sender) > 1:
             raise ValueError('You must not specify more than one sender!')
-
+        
         if not self._dirty and self._processed:
             return self._mime
-
+        
         self._processed = False
-
+        
         plain = MIMEText(self._callable(self.plain), 'plain', self.encoding)
-
+        
         rich = None
         if self.rich:
             rich = MIMEText(self._callable(self.rich), 'html', self.encoding)
-
+        
         message = self.mime_document(plain, rich)
         headers = self._build_header_list(author, sender)
         self._add_headers_to_message(message, headers)
-
+        
         self._mime = message
         self._processed = True
         self._dirty = False
-
+        
         return message
-
+    
     def attach(self, name, data=None, maintype=None, subtype=None, inline=False):
         """Attach a file to this message.
-
+        
         :param name: Path to the file to attach if data is None, or the name
                      of the file if the ``data`` argument is given
         :param data: Contents of the file to attach, or None if the data is to
@@ -248,7 +254,7 @@ class Message(object):
             part.set_payload(data.read())
         else:
             raise TypeError("Unable to read attachment contents")
-
+        
         if inline:
             part.add_header('Content-Disposition', 'inline', filename=name)
             part.add_header('Content-ID', '<%s>' % name)
@@ -256,12 +262,12 @@ class Message(object):
         else:
             part.add_header('Content-Disposition', 'attachment', filename=name)
             self.attachments.append(part)
-
+    
     def embed_image(self, name, data=None):
         """Attach an image file and prepare for HTML embedding.
-
+        
         This method should only be used to embed images.
-
+        
         :param name: Path to the image to embed if data is None, or the name
                      of the file if the ``data`` argument is given
         :param data: Contents of the image to embed, or None if the data is to
@@ -277,12 +283,13 @@ class Message(object):
             data = data.read()
         else:
             raise TypeError("Unable to read image contents")
-
+        
         subtype = imghdr.what(None, data)
         self.attach(name, data, 'image', subtype, True)
-
+    
     @staticmethod
     def _callable(var):
-        if callable(var):
+        if hasattr(var, '__call__'):
             return var()
+        
         return var
