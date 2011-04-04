35c94fd400109bfd5f6f26297fd8e959dc91ab83
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-04-04 16:43:56 -0700

    Added sendmail transport.

--- marrow/mailer/transport/sendmail.py
+++ marrow/mailer/transport/sendmail.py
@@ -1,19 +1,31 @@
 # encoding: utf-8
 
+import os
+
+from marrow.mailer.exc import DeliveryFailedException
+
+
 __all__ = ['SendmailTransport']
 
 log = __import__('logging').getLogger(__name__)
 
 
+
 class SendmailTransport(object):
-	def __init__(self, **kw):
-		self.config = kw
-	
-	def startup(self):
-		pass
-	
-	def __call__(self, message):
-		pass # Deliver the message.
-	
-	def shutdown(self):
-		pass
+    def __init__(self, config):
+        self.executable = config.get('path', '/usr/sbin/sendmail')
+    
+    def startup(self):
+        pass
+    
+    def __call__(self, message):
+        # TODO: Utilize -F full_name (sender full name), -f sender (envelope sender), -V envid (envelope ID), and space-separated BCC recipients
+        proc = os.popen('%s -t -i' % (self.executable, ), 'w')
+        proc.write(bytes(message))
+        status = proc.close()
+        
+        if status != 0:
+            raise DeliveryFailedException("Sendmail delivery failed with status code %d." % (status, ), status)
+    
+    def shutdown(self):
+        pass
