7d1beba17b40d595a0073858f431d87c456b49db
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-04-04 18:20:54 -0700

    Initial IMAP transport; needs error condition handling.

--- marrow/mailer/transport/imap.py
+++ marrow/mailer/transport/imap.py
@@ -1,19 +1,35 @@
 # encoding: utf-8
 
+import imaplib
+
+from datetime import datetime
+
+
 __all__ = ['IMAPTransport']
 
 log = __import__('logging').getLogger(__name__)
 
 
+
 class IMAPTransport(object):
-	def __init__(self, **kw):
-		self.config = kw
-	
-	def startup(self):
-		pass
-	
-	def __call__(self, message):
-		pass # Deliver the message.
-	
-	def shutdown(self):
-		pass
+    def __init__(self, config):
+        self.host = config.get('host', None)
+        self.ssl = config.get('ssl', False)
+        self.port = config.get('port', 993 if self.ssl else 143)
+        self.username = config.get('username', None)
+        self.password = config.get('password', None)
+        self.folder = config.get('folder', "INBOX")
+    
+    def startup(self):
+        Protocol = imaplib.IMAP4_SSL if self.ssl else imaplib.IMAP4
+        self.connection = Protocol(self.host, self.port)
+        
+        if self.username:
+            result = self.connection.login(self.username, self.password)
+            log.debug("Response: %r", result)
+    
+    def __call__(self, message):
+        self.connection.append(self.folder, '', message.date if message.date else datetime.now(), bytes(message))
+    
+    def shutdown(self):
+        self.connection.logout()
