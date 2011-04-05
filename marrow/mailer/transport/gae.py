f7e1b3db4afc0923beddc99a87b964a9a72f8ec9
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-04-04 22:51:11 -0700

    Added Google App Engine stub.

---
+++ marrow/mailer/transport/gae.py
@@ -0,0 +1,2 @@
+# TODO: Translate between our Message object and http://code.google.com/appengine/docs/python/mail/emailmessagefields.html
+# Deliver using http://code.google.com/appengine/docs/python/mail/sendingmail.html
