9d23b1a41b0b6bccff642d581565eb2216c6cf2e
Alice Bevan-McGregor <alice@gothcandy.com>
2011-07-03 03:36:32 -0400

    What the hell, I'll throw in an Amazon SES transport tonight, too.

--- marrow/mailer/transport/ses.py
+++ marrow/mailer/transport/ses.py
@@ -2,6 +2,12 @@
 
 # TODO: Port: https://github.com/pankratiev/python-amazon-ses-api/blob/master/amazon_ses.py
 
+try:
+    from boto.ses import SESConnection
+
+except ImportError:
+    raise ImportError("You must install the boto package to deliver mail via Amazon SES.")
+
 
 __all__ = ['AmazonTransport']
 
@@ -11,13 +17,37 @@ log = __import__('logging').getLogger(__name__)
 
 class AmazonTransport(object):
     def __init__(self, config):
-        pass
+        self.id = config.get('id')
+        self.key = config.get('key')
+        self.host = config.get('host', "email.us-east-1.amazonaws.com")
+        self.connection = None
     
     def startup(self):
-        pass
+        self.connection = SESConnection(
+                aws_access_key_id = self.id,
+                aws_secret_access_key = self.key,
+                host = self.host
+            )
     
     def deliver(self, message):
-        pass # Deliver the message.
+        try:
+            response = self.connection.send_raw_email(
+                    source = message.author.encode(),
+                    destinations = message.recipients.encode(),
+                    raw_message = str(message)
+                )
+            
+            return (
+                    response['SendRawEmailResponse']['SendRawEmailResult']['MessageId'],
+                    response['SendRawEmailResponse']['ResponseMetadata']['RequestId']
+                )
+        
+        except SESConnection.ResponseError, err:
+            raise
+            # ['status', 'reason', 'body', 'request_id', 'error_code', 'error_message']
     
     def shutdown(self):
-        pass
+        if self.connection:
+            self.connection.close()
+        
+        self.connection = None
