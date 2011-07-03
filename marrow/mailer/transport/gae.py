a519ea5894a43365c5503bd0fcc2303d915407d6
Alice Bevan-McGregor <alice@gothcandy.com>
2011-07-03 03:10:44 -0400

    Initial GAE transport; requires testing… somehow.

--- marrow/mailer/transport/gae.py
+++ marrow/mailer/transport/gae.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+from google.appengine.api import mail
+
 
 __all__ = ['AppEngineTransport']
 
@@ -18,7 +20,37 @@ class AppEngineTransport(object):
         pass
     
     def deliver(self, message):
-        pass # Deliver the message.
+        msg = mail.EmailMessage(
+                sender = message.author.encode(),
+                to = [to.encode() for to in message.to],
+                subject = message.subject,
+                body = message.plain
+            )
+        
+        if message.cc:
+            msg.cc = [cc.encode() for cc in message.cc]
+        
+        if message.bcc:
+            msg.bcc = [bcc.encode() for bcc in message.bcc]
+        
+        if message.reply:
+            msg.reply_to = message.reply.encode()
+        
+        if message.rich:
+            msg.html = message.rich
+        
+        if message.attachments:
+            attachments = []
+            
+            for attachment in message.attachments:
+                attachments.append((
+                        attachment['Content-Disposition'].partition(';')[2],
+                        attachment.get_payload(True)
+                    ))
+            
+            msg.attachments = attachments
+        
+        msg.send()
     
     def shutdown(self):
         pass
