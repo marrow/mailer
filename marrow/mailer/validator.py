5d3bc25f8765548ed55be48b0466e3eafa460594
Alice Bevan-McGregor <alice@gothcandy.com>
2011-11-15 13:45:22 -0500

    Complete unit tests for address and domain validation, fixes #27.

--- marrow/mailer/validator.py
+++ marrow/mailer/validator.py
@@ -100,7 +100,7 @@ Code written in 2009 and donated to the public domain.
 
 import re
 
-__all__ = ['BaseValidator', 'ValidationException', 'EmailValidator']
+__all__ = ['ValidationException', 'BaseValidator', 'DomainValidator', 'EmailValidator', 'EmailHarvester']
 
 
 class ValidationException(ValueError):
@@ -113,12 +113,13 @@ class BaseValidator(object):
         "Don't return success codes, use exceptions!"
         This method allows them to be happy, too.
         """
-
-        validatee, err = self.validate(*a, **k)
+        
+        validate, err = self.validate(*a, **k)
+        
         if err:
             raise ValidationException(err)
-        else:
-            return validatee
+        
+        return validate
 
 
 class DomainValidator(BaseValidator):
@@ -143,10 +144,17 @@ class DomainValidator(BaseValidator):
 
     def __init__(self, fix=False, lookup_dns=None):
         self.fix = fix
+        
         if lookup_dns:
+            try:
+                import DNS
+            except ImportError: # pragma: no cover
+                raise ImportError("To enable DNS lookup of domains install the PyDNS package.")
+            
             lookup_dns = lookup_dns.lower()
-            if not lookup_dns == 'a' and not lookup_dns == 'mx':
+            if lookup_dns not in ('a', 'mx'):
                 raise RuntimeError("Not a valid *lookup_dns* value: " + lookup_dns)
+        
         self._lookup_dns = lookup_dns
 
     def _apply_common_rules(self, part, maxlength):
@@ -154,37 +162,47 @@ class DomainValidator(BaseValidator):
         domain and the local part of the e-mail address.
         """
         part = part.strip()
+        
         if self.fix:
             part = part.strip('.')
+        
         if not part:
             return part, 'It cannot be empty.'
+        
         if len(part) > maxlength:
             return part, 'It cannot be longer than %i chars.' % maxlength
+        
         if part[0] == '.':
             return part, 'It cannot start with a dot.'
+        
         if part[-1] == '.':
             return part, 'It cannot end with a dot.'
+        
         if '..' in part:
             return part, 'It cannot contain consecutive dots.'
+        
         return part, ''
 
     def validate_domain(self, part):
         part, err = self._apply_common_rules(part, maxlength=255)
+        
         if err:
             return part, 'Invalid domain: %s' % err
+        
         if not self.domain_regex.search(part):
             return part, 'Invalid domain.'
+        
         if self._lookup_dns and not self.lookup_domain(part):
             return part, 'Domain does not seem to exist.'
-        else:
-            return part.lower(), ''
+        
+        return part.lower(), ''
 
     validate = validate_domain
 
     # TODO: As an option, DNS lookup on the domain:
     # http://mail.python.org/pipermail/python-list/2008-July/497997.html
 
-    def lookup_domain(self, domain, lookup_record=None):
+    def lookup_domain(self, domain, lookup_record=None, **kw):
         """Looks up the DNS record for *domain* and returns:
         
         * None if it does not exist,
@@ -198,42 +216,40 @@ class DomainValidator(BaseValidator):
         "a" means verify that the domain exists.
         "mx" means verify that the domain exists and specifies mail servers.
         """
-        if lookup_record:
-            lookup_record = lookup_record.lower()
-        else:
-            lookup_record = self._lookup_dns
-        result = None
+        import DNS
+        
+        lookup_record = lookup_record.lower() if lookup_record else self._lookup_dns
+        
+        if lookup_record not in ('a', 'mx'):
+            raise RuntimeError("Not a valid lookup_record value: " + lookup_record)
+        
         if lookup_record == "a":
-            request = DNS.Request(domain)
+            request = DNS.Request(domain, **kw)
+            
             try:
                 answers = request.req().answers
-            except DNS.Lib.PackError:
+            
+            except (DNS.Lib.PackError, UnicodeError):
                 # A part of the domain name is longer than 63.
                 return False
-                # raise RuntimeError(err)
-            # print repr(answers)
-            if answers:
-                result = answers[0]['data'] # This is an IP address
-                if result in self.false_positive_ips:
-                    result = None
-                    # print "Domain '%s' not found" % domain 
-                else:
-                    # print "Domain '%s' found with address: %s" \
-                    #    % (domain, result)
-                    pass
-            else:
-                # print "Domain '%s' not found" % domain
-                pass
-        elif lookup_record == "mx":
-            result = DNS.mxlookup(domain) # this is a list of mx records
-            #if result:
-            #    print "Domain '%s' has MX records: %s" % (domain, result)
-            #else:
-            #    print "Domain '%s' has no MX records." % domain
-        else:
-            raise RuntimeError("Not a valid lookup_record value: " \
-                               + lookup_record)
-        return result
+            
+            if not answers:
+                return False
+            
+            result = answers[0]['data'] # This is an IP address
+            
+            if result in self.false_positive_ips: # pragma: no cover
+                return False
+            
+            return result
+        
+        try:
+            return DNS.mxlookup(domain)
+        
+        except UnicodeError:
+            pass
+        
+        return False
 
 
 class EmailValidator(DomainValidator):
@@ -246,11 +262,9 @@ class EmailValidator(DomainValidator):
     def __init__(self, local_part_chars=".-+_!#$%&'/=`|~?^{}*", **k):
         super(EmailValidator, self).__init__(**k)
         # Add a backslash before the dash so it can go into the regex:
-        self.local_part_pattern = '[a-z0-9' \
-            + local_part_chars.replace('-', r'\-') + ']+'
+        self.local_part_pattern = '[a-z0-9' + local_part_chars.replace('-', r'\-') + ']+'
         # Regular expression for validation:
-        self.local_part_regex = \
-            re.compile('^' + self.local_part_pattern + '$', re.IGNORECASE)
+        self.local_part_regex = re.compile('^' + self.local_part_pattern + '$', re.IGNORECASE)
 
     def validate_local_part(self, part):
         part, err = self._apply_common_rules(part, maxlength=64)
@@ -264,21 +278,24 @@ class EmailValidator(DomainValidator):
     def validate_email(self, email):
         if not email:
             return email, 'The e-mail is empty.'
+        
         parts = email.split('@')
+        
         if len(parts) != 2:
             return email, 'An email address must contain a single @'
+        
         local, domain = parts
-
+        
         # Validate the domain
         domain, err = self.validate_domain(domain)
         if err:
-            return email, \
-                   "The e-mail has a problem to the right of the @: %s" % err
+            return email, "The e-mail has a problem to the right of the @: %s" % err
+        
         # Validate the local part
         local, err = self.validate_local_part(local)
         if err:
-            return email, \
-                   "The email has a problem to the left of the @: %s" % err
+            return email, "The email has a problem to the left of the @: %s" % err
+        
         # It is valid
         return local + '@' + domain, ''
 
@@ -301,42 +318,42 @@ class EmailHarvester(EmailValidator):
             yield match.group().replace('..', '.')
 
 
-rfc822_specials = '()<>@,;:\\"[]'
+# rfc822_specials = '()<>@,;:\\"[]'
 
-def is_address_valid(addr):
-    # First we validate the name portion (name@domain)
-    c = 0
-    while c < len(addr):
-        if addr[c] == '"' and (not c or addr[c - 1] == '.' or addr[c - 1] == '"'):
-            c = c + 1
-            while c < len(addr):
-                if addr[c] == '"': break
-                if addr[c] == '\\' and addr[c + 1] == ' ':
-                    c = c + 2
-                    continue
-                if ord(addr[c]) < 32 or ord(addr[c]) >= 127: return 0
-                c = c + 1
-            else: return 0
-            if addr[c] == '@': break
-            if addr[c] != '.': return 0
-            c = c + 1
-            continue
-        if addr[c] == '@': break
-        if ord(addr[c]) <= 32 or ord(addr[c]) >= 127: return 0
-        if addr[c] in rfc822_specials: return 0
-        c = c + 1
-    if not c or addr[c - 1] == '.': return 0
-
-    # Next we validate the domain portion (name@domain)
-    domain = c = c + 1
-    if domain >= len(addr): return 0
-    count = 0
-    while c < len(addr):
-        if addr[c] == '.':
-            if c == domain or addr[c - 1] == '.': return 0
-            count = count + 1
-        if ord(addr[c]) <= 32 or ord(addr[c]) >= 127: return 0
-        if addr[c] in rfc822_specials: return 0
-        c = c + 1
-
-    return count >= 1
+# is_address_valid(addr):
+# # First we validate the name portion (name@domain)
+# c = 0
+# while c < len(addr):
+#     if addr[c] == '"' and (not c or addr[c - 1] == '.' or addr[c - 1] == '"'):
+#         c = c + 1
+#         while c < len(addr):
+#             if addr[c] == '"': break
+#             if addr[c] == '\\' and addr[c + 1] == ' ':
+#                 c = c + 2
+#                 continue
+#             if ord(addr[c]) < 32 or ord(addr[c]) >= 127: return 0
+#             c = c + 1
+#         else: return 0
+#         if addr[c] == '@': break
+#         if addr[c] != '.': return 0
+#         c = c + 1
+#         continue
+#     if addr[c] == '@': break
+#     if ord(addr[c]) <= 32 or ord(addr[c]) >= 127: return 0
+#     if addr[c] in rfc822_specials: return 0
+#     c = c + 1
+# if not c or addr[c - 1] == '.': return 0
+# 
+# # Next we validate the domain portion (name@domain)
+# domain = c = c + 1
+# if domain >= len(addr): return 0
+# count = 0
+# while c < len(addr):
+#     if addr[c] == '.':
+#         if c == domain or addr[c - 1] == '.': return 0
+#         count = count + 1
+#     if ord(addr[c]) <= 32 or ord(addr[c]) >= 127: return 0
+#     if addr[c] in rfc822_specials: return 0
+#     c = c + 1
+# 
+# return count >= 1
