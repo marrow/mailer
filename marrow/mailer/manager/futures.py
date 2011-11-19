a6b189d4ed108b51cdbddf40d01368e0e1d98ddf
Alice Bevan-McGregor <alice@gothcandy.com>
2011-11-18 19:39:16 -0500

    Full coverage for the futures manager; fixes #16.

--- marrow/mailer/manager/futures.py
+++ marrow/mailer/manager/futures.py
@@ -7,7 +7,7 @@ from marrow.mailer.manager.util import TransportPool
 
 try:
     from concurrent import futures
-except ImportError:
+except ImportError: # pragma: no cover
     raise ImportError("You must install the futures package to use background delivery.")
 
 
@@ -25,8 +25,7 @@ def worker(pool, message):
             try:
                 result = transport.deliver(message)
             
-            except MessageFailedException:
-                e = sys.exc_info()[1]
+            except MessageFailedException as e:
                 raise DeliveryFailedException(message, e.args[0] if e.args else "No reason given.")
             
             except TransportFailedException:
