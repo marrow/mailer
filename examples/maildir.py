3e73500c691c4bf95255a3cd6f98c82ff4d6d491
Alice Bevan-McGregor <alice@gothcandy.com>
2011-11-22 10:29:11 -0500

    Examples now use the correct form of reference for managers and transports; fixes #31.

--- examples/maildir.py
+++ examples/maildir.py
@@ -2,7 +2,7 @@ import logging
 from marrow.mailer import Message, Delivery
 logging.basicConfig(level=logging.INFO)
 
-mail = Delivery({'manager': 'immediate', 'transport': 'maildir', 'transport.directory': 'data/maildir'})
+mail = Delivery({'manager.use': 'immediate', 'transport.use': 'maildir', 'transport.directory': 'data/maildir'})
 mail.start()
 
 message = Message([('Alice Bevan-McGregor', 'alice@gothcandy.com')], [('Alice Two', 'alice.mcgregor@me.com')], "This is a test message.", plain="Testing!")
