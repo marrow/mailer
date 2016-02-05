a88a2609ec0396a795a0f267e099c1bb9b26fc50
Alice Bevan–McGregor <alice@gothcandy.com>
2016-02-04 21:37:37 -0500

    Updates for changes in boto API.
    Also now allows for passing additional configuration through, albeit
    with their verbose names.  Thanks to @tudalex on GitHub for the initial
    fix.

--- marrow/mailer/transport/ses.py
+++ marrow/mailer/transport/ses.py
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-# TODO: Port: https://github.com/pankratiev/python-amazon-ses-api/blob/master/amazon_ses.py
-
 try:
     import boto.ses 
     from boto.ses import SESConnection
@@ -17,29 +15,24 @@ log = __import__('logging').getLogger(__name__)
 
 
 class AmazonTransport(object): # pragma: no cover
-    __slots__ = ('ephemeral', 'id', 'key', 'region', 'connection')
+    __slots__ = ('ephemeral', 'config', 'region', 'connection')
     
     def __init__(self, config):
-        self.id = config.get('id')
-        self.key = config.get('key')
-        self.region = config.get('region', "us-east-1")
+        # Give our configuration aliases their proper names.
+        config['aws_access_key_id'] = config.pop('id')
+        config['aws_secret_access_key'] = config.pop('key')
+        
+        self.region = config.pop('region', "us-east-1")
+        self.config = config  # All other configuration directives are passed to connect_to_region.
         self.connection = None
     
     def startup(self):
-        self.connection = boto.ses.connect_to_region(self.region,
-                aws_access_key_id = self.id,
-                aws_secret_access_key = self.key
-            )
+        self.connection = boto.ses.connect_to_region(self.region, **self.config)
     
     def deliver(self, message):
         try:
             destinations = [r.encode(encoding='utf-8') for r in message.recipients]
-
-            response = self.connection.send_raw_email(
-                    source = message.author.encode(),
-                    destinations = destinations,
-                    raw_message = str(message)
-                )
+            response = self.connection.send_raw_email(str(message), message.author.encode(), destinations)
             
             return (
                     response['SendRawEmailResponse']['SendRawEmailResult']['MessageId'],
