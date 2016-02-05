9003414d5e8ed7733fc113b9acc55eb8ab4d0eff
Alice Bevan–McGregor <alice@gothcandy.com>
2016-02-05 03:17:11 -0500

    Removed tox. What a pain.

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,8 +6,6 @@ git config --global user.email "alice+travis@gothcandy.com"
 git config --global user.name "Travis: Marrow"
 
 pip install --upgrade pytest
-pip install tox
-pip install tox-travis
 pip install codecov
 pip install pytest-cov
 pip install pyflakes
