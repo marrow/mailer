ce92ad4a5776df1917efa2a92f08cbe535d9c878
Alice Bevan–McGregor <alice@gothcandy.com>
2016-02-04 23:44:40 -0500

    Updated packaging and test configuration to conform to current Marrow standards.

--- marrow/mailer/release.py
+++ marrow/mailer/release.py
@@ -1,13 +1,14 @@
 # encoding: utf-8
 
-"""Release information about Marrow Mail."""
+"""Release information about Marrow Mailer."""
 
 from collections import namedtuple
 
 
-__all__ = ['version_info', 'version']
-
-
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(4, 0, 1, 'final', 0)
-
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(4, 1, 0, 'beta', 1)
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+
+author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
+
+description = "A highly efficient and modular mail delivery framework for Python 2.6+ and 3.2+."
+url = 'https://github.com/marrow/mailer/'
