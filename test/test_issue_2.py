7fc42424cef1a3a4cf5c5449068023f42dddf191
Alice Bevan-McGregor <alice@gothcandy.com>
2019-09-15 22:40:15 -0400

    %retab!

--- test/test_issue_2.py
+++ test/test_issue_2.py
@@ -1,14 +1,13 @@
 from marrow.mailer import Mailer
 
 
-
 def test_issue_2():
-    mail = Mailer({
-            'manager.use': 'immediate',
-            'transport.use': 'smtp',
-            'transport.host': 'secure.emailsrvr.com',
-            'transport.tls': 'ssl'
-        })
-    
-    mail.start()
-    mail.stop()
+	mail = Mailer({
+			'manager.use': 'immediate',
+			'transport.use': 'smtp',
+			'transport.host': 'secure.emailsrvr.com',
+			'transport.tls': 'ssl'
+		})
+	
+	mail.start()
+	mail.stop()
