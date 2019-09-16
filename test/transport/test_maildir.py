a23709fbf372266bb0a2e26d8b62b6235d749e52
Alice Bevan-McGregor <alice@gothcandy.com>
2019-09-15 22:42:20 -0400

    %retab!

--- test/transport/test_maildir.py
+++ test/transport/test_maildir.py
@@ -18,43 +18,43 @@ log = logging.getLogger('tests')
 
 
 class TestMailDirectoryTransport(TestCase):
-    def setUp(self):
-        self.path = tempfile.mkdtemp()
-        
-        for i in ('cur', 'new', 'tmp'):
-            os.mkdir(os.path.join(self.path, i))
-        
-        self.transport = MaildirTransport(dict(directory=self.path, create=True))
-    
-    def tearDown(self):
-        self.transport.shutdown()
-        shutil.rmtree(self.path)
-    
-    def test_bad_config(self):
-        self.assertRaises(ValueError, MaildirTransport, dict())
-    
-    def test_startup(self):
-        self.transport.startup()
-        self.assertTrue(isinstance(self.transport.box, mailbox.Maildir))
-    
-    def test_child_folder_startup(self):
-        self.transport.folder = 'test'
-        self.transport.startup()
-        self.assertTrue(os.path.exists(os.path.join(self.path, '.test')))
-    
-    def test_shutdown(self):
-        self.transport.startup()
-        self.transport.shutdown()
-        self.assertTrue(self.transport.box is None)
-    
-    def test_delivery(self):
-        message = Message('from@example.com', 'to@example.com', "Test subject.")
-        message.plain = "Test message."
-        
-        self.transport.startup()
-        self.transport.deliver(message)
-        
-        filename = os.listdir(os.path.join(self.path, 'new'))[0]
-        
-        with open(os.path.join(self.path, 'new', filename), 'rb') as fh:
-            self.assertEqual(str(message), fh.read())
+	def setUp(self):
+		self.path = tempfile.mkdtemp()
+		
+		for i in ('cur', 'new', 'tmp'):
+			os.mkdir(os.path.join(self.path, i))
+		
+		self.transport = MaildirTransport(dict(directory=self.path, create=True))
+	
+	def tearDown(self):
+		self.transport.shutdown()
+		shutil.rmtree(self.path)
+	
+	def test_bad_config(self):
+		self.assertRaises(ValueError, MaildirTransport, dict())
+	
+	def test_startup(self):
+		self.transport.startup()
+		self.assertTrue(isinstance(self.transport.box, mailbox.Maildir))
+	
+	def test_child_folder_startup(self):
+		self.transport.folder = 'test'
+		self.transport.startup()
+		self.assertTrue(os.path.exists(os.path.join(self.path, '.test')))
+	
+	def test_shutdown(self):
+		self.transport.startup()
+		self.transport.shutdown()
+		self.assertTrue(self.transport.box is None)
+	
+	def test_delivery(self):
+		message = Message('from@example.com', 'to@example.com', "Test subject.")
+		message.plain = "Test message."
+		
+		self.transport.startup()
+		self.transport.deliver(message)
+		
+		filename = os.listdir(os.path.join(self.path, 'new'))[0]
+		
+		with open(os.path.join(self.path, 'new', filename), 'rb') as fh:
+			self.assertEqual(str(message), fh.read())
