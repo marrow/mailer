ae674c8f8043e0ea0e6f45f58eccb5d0f16a42ea
Alice Bevan-McGregor <alice@gothcandy.com>
2019-09-15 22:41:01 -0400

    Prospective version bump.

--- marrow/mailer/release.py
+++ marrow/mailer/release.py
@@ -3,7 +3,7 @@
 from collections import namedtuple
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(4, 0, 3, 'final', 0)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(5, 0, 0, 'alpha', 1)
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
 
 author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
